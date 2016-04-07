cadcfdd9c8f71da3007938883a1f4a521118e9a7
大橋　勇 <y_ohashi@waku-2.com>
2016-04-07 17:54:12 +0900

    gulp-connect削除（非推奨の為）

--- gulpfile.js
+++ gulpfile.js
@@ -3,30 +3,23 @@ const gulp = require('gulp');
 const babel = require('gulp-babel');
 const plumber = require('gulp-plumber');
 const connect = require('gulp-connect');
-const browserSync = require('browser-sync');
 const webserver = require('gulp-webserver');
 const uglify = require('gulp-uglify');
 
-const reload = browserSync.reload();
-
 /*
  * 変更を監視してブラウザリロード
+ *
+ * livereload: ture（ライブリロード実行）
+ * open: true（実行と同時に）
  */
-gulp.task('browser-sync', () => {
-	browserSync.init(null, {
-		server: {
-			baseDir: './'
-		}
-	});
-	gulp.watch(['./app/**'], reload);
-});
-
-gulp.task('webserver', () => {
+gulp.task('webserver', function() {
 	gulp.src('./app')
 		.pipe(webserver({
 			host: 'localhost',
 			port: 8080,
-			livereload: true
+			livereload: true,
+			directoryListing: true,
+			open: true
 		})
 	);
 });
@@ -34,22 +27,22 @@ gulp.task('webserver', () => {
 /*
  * localhostサーバー
  */
-gulp.task('connect', () => {
+gulp.task('connect', function() {
 	connect.server({
-		root: 'app',
+		root: './app',
 		livereload: true
 	});
 });
 
-gulp.task('html', () => {
+gulp.task('html', function() {
 	gulp.src('./*.html').pipe(connect.reload());
 });
 
 /*
  * Babel
  */
-gulp.task('babel', () => {
-	return gulp.src('./app/main.js')
+gulp.task('babel', function() {
+	return gulp.src('./app/*.js')
 	.pipe(plumber())
 	.pipe(babel({
 		presets: ['es2015']
@@ -60,16 +53,18 @@ gulp.task('babel', () => {
 /*
  * ファイルの変更を監視
  */
-gulp.task('watch', () => {
-	gulp.watch('./**/*.js', ['babel']);
+gulp.task('watch', function() {
+	gulp.watch('./dist/*.js', ['babel']);
 });
 
 /*
  * ミニファイ化
  */
-gulp.task('uglify', () => {
+gulp.task('uglify', function() {
 	gulp.src('./');
 });
 
-
-gulp.task('default', ['browser-sync', 'connect', 'babel', 'watch']);
+/*
+ * デフォルトタスク
+ */
+gulp.task('default', ['webserver', 'connect', 'babel', 'watch']);
